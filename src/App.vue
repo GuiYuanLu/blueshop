91cffe8f3ae2b5431d29659bd39b38f5c00706bb
Rayi Rui Liu <rayi_rui_liu@apple.com>
2023-12-21 11:37:19 +0800

    import Element plus and set  scss styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <h1>hello world</h1>
+    <h1 class="red">hello world</h1>
     <RouterView />
+    <h2>Ray</h2>
+    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 <script setup>
 import { RouterView } from 'vue-router'
 </script>
 
-<style scoped></style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+import '@/styles/index.scss'
 
 app.use(createPinia())
+
 app.use(router)
 
 app.mount('#app')
